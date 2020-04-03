96f4d6bebbab61e56288c1aecccb28f78f5eaa97
Hong-Jun-Ho <57649418+Hong-Jun-Ho@users.noreply.github.com>
2020-04-03 14:08:29 +0900

    문자열 내림차순으로 배치하기

---
+++ venv/Programmers/Descending.py
@@ -0,0 +1,20 @@
+def solution(s):
+    q = []
+    for c in range(len(s)):
+        q.append(ord(s[c]))
+    q.sort()
+    q.reverse()
+
+    for c in range(len(q)):
+        q[c] =(chr(q[c]))
+
+    result = ''.join(q)
+    return result
+
+print(solution("Zbcdefg"))
+
+
+## 다른사람의 풀이
+
+## def solution(s):
+##     return ''.join(sorted(s, reverse=True))
