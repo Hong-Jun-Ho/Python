cd4c2ce31e3e3809c4fcd07b4750097acbcb829f
Hong-Jun-Ho <57649418+Hong-Jun-Ho@users.noreply.github.com>
2020-04-21 09:46:00 +0900

    프로그래머스 level1

---
+++ venv/Programmers/level1/prime.py
@@ -0,0 +1,23 @@
+
+def is_prime_not_bad(n: int) -> bool:
+    if n < 2:
+        return False
+    if n is 2:
+        return True
+    if n % 2 is 0:
+        return False
+    l = round(n ** 0.5) + 1
+    for i in range(3, l, 2):
+        if n % i is 0:
+            return False
+    return True
+
+def solution(n):
+    count =0
+    for e in range(n+1):
+        if is_prime_not_bad(e) == True:
+            count += 1
+    return count
+
+
+print(solution(10))
