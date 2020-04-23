ce16fa905799e4fcc2f94e6209e3e2c6885f436a
Hong-Jun-Ho <57649418+Hong-Jun-Ho@users.noreply.github.com>
2020-04-23 14:44:26 +0900

    20200423 level1 스터디

---
+++ venv/Programmers/level1/Sortmyway.py
@@ -0,0 +1,18 @@
+import operator
+
+
+def solution(strings, n):
+    answer = []
+    Dict = {}
+    # ...1
+    for i in strings:
+        Dict[i] = i[n]
+
+    sortedDict = sorted(Dict.items(), key=operator.itemgetter(1, 0))
+
+    for i in sortedDict:
+        answer.append(i[0])
+    return answer
+
+
+print(solution(["abce", "abcd", "cdx"], 2))
