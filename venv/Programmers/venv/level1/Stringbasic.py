8364de3127ed599863cf9b514260c048212be827
Hong-Jun-Ho <57649418+Hong-Jun-Ho@users.noreply.github.com>
2020-04-23 14:54:09 +0900

    2016년 요일 구하기

--- venv/Programmers/venv/level1/Stringbasic.py
+++
@@ -1,9 +0,0 @@
-def solution(s):
-    if len(s) != 4 and len(s) != 6:
-        return False
-    try:
-        int(s)
-    except ValueError:
-        return False
-
-    return True
--- venv/Programmers/venv/level1/Watermelon.py
+++
@@ -1,6 +0,0 @@
-def solution(n):
-    a = "수박"
-    b = a * (n // 2 + 1)
-    answer = b[:n]
-
-    return answer
