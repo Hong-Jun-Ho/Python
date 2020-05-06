583c53002feb755f42f71acf53b0675cb060c4eb
Hong-Jun-Ho <57649418+Hong-Jun-Ho@users.noreply.github.com>
2020-05-07 01:14:21 +0900

    TOP

---
+++ venv/Programmers/level2/Top.py
@@ -0,0 +1,14 @@
+def solution(heights):
+    answer = []
+    for p_index in range(len(heights)-1, -1, -1):
+        Check = False
+        for c_index in range(p_index-1, -1, -1):
+            if heights[p_index] < heights[c_index]:
+                Check = True
+            answer = [c_index + 1] + answer
+            break
+        if not Check:
+            answer = [0] + answer
+    return answer
+
+print(solution([6, 9, 5, 7, 4]))
