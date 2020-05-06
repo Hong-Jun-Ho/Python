44e6e6dee574c2f6267f4d61a9d07ef64b3e9a95
Hong-Jun-Ho <57649418+Hong-Jun-Ho@users.noreply.github.com>
2020-05-07 01:17:57 +0900

    TOP

--- venv/Programmers/level2/Top.py
+++ venv/Programmers/level2/Top.py
@@ -5,8 +5,8 @@ def solution(heights):
         for c_index in range(p_index-1, -1, -1):
             if heights[p_index] < heights[c_index]:
                 Check = True
-            answer = [c_index + 1] + answer
-            break
+                answer = [c_index + 1] + answer
+                break
         if not Check:
             answer = [0] + answer
     return answer
