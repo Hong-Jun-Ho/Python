58119f7cfd138a3992182e9118472dd70f85e7f2
Hong-Jun-Ho <57649418+Hong-Jun-Ho@users.noreply.github.com>
2020-05-07 15:34:02 +0900

    5/7 Study X interval N

--- venv/Programmers/level1/XintervalN.py
+++ venv/Programmers/level1/XintervalN.py
@@ -3,13 +3,15 @@
 ## 정수 x와 자연수 n을 입력 받는다
 ## x부터 시작해 x씩 증가하는 숫자를 n 개 지니는 리스트 리턴
 ## 문제풀이
-## for 문안에 x+=x 를 하면 끝
+## for 문안에 y+=x 를 하면 끝
 ## x for happy
 def solution(x, n):
     answer = []
+    y=0
     for e in range(n):
-        x+=x
-        answer.append(x)
+        y+=x
+        answer.append(y)
+
     return answer
 
-print(solution(-2,5))
+print(solution(2,5))
