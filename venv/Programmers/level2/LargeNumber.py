889c75341ad2491dd52e0344c7f0453eb11d3cfa
Hong-Jun-Ho <57649418+Hong-Jun-Ho@users.noreply.github.com>
2020-08-04 15:19:08 +0900

    8/4 study

--- venv/Programmers/level2/LargeNumber.py
+++ venv/Programmers/level2/LargeNumber.py
@@ -23,21 +23,23 @@
 
 def solution(number, k):
     num_list = []
+    check_len = len(number) - k
 
     for index, num in enumerate(number):
-        if len(num_list) > 0:
-            while k > 0 and num_list[-1] < num:
-                num_list.pop()
-                k -= 1
-                if len(num_list) == 0:
-                    break
+        while len(num_list) > 0 and k > 0 and num_list[-1] < num:
+            num_list.pop()
+            k -= 1
         if k == 0:
             num_list += list(number[index:])
             break
         num_list.append(num)
 
     answer = ''.join(num_list)
+
+    if len(answer) > check_len:
+        answer = answer[:check_len]
+
     return answer
 
 
-print(solution("4177252841", 4))
+print(solution("319999", 3))
