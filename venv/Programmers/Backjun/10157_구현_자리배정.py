27c90966ba2b5cf8229852a3fbb3707955a376db
Hong-Jun-Ho <57649418+Hong-Jun-Ho@users.noreply.github.com>
2020-09-24 16:21:07 +0900

    9/24 Study

--- venv/Programmers/Backjun/10157_구현_자리배정.py
+++ venv/Programmers/Backjun/10157_구현_자리배정.py
@@ -50,28 +50,33 @@
 
 
 def Within(lst, y, x, c, r):
-    if x >= 0 and x < c and y >= 0 and y < r:
-        if lst[y][x] == 0: return True
+    if 0 <= x < c and 0 <= y < r:
+        if lst[y][x] == 0:
+            return True
     return False
+
 
 C, R = map(int, input().split())
 person = int(input())
 
+# 상, 우, 하, 좌
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
 idx = 0
-x, y = 0, R-1
+x, y = 0, R - 1
 field = [[0 for i in range(C)] for j in range(R)]
 
-for i in range(C*R):
-    field[y][x] = i+1
-    if person > C*R:
+for i in range(C * R):
+    field[y][x] = i + 1
+    if person > C * R:
         print(0)
         break
-    elif i+1 == person:
-        print('{0} {1}'.format(x+1, R-y))
+    elif i + 1 == person:
+        print('{0} {1}'.format(x + 1, R - y))
         break
-    if not Within(field, y+dy[idx], x+dx[idx], C, R):
+
+    ## 자리를 벗어나는 index 값인지 이미 배정되어 있는 사람인지 확인
+    if not Within(field, y + dy[idx], x + dx[idx], C, R):
         idx = (idx + 1) % 4
     x += dx[idx]
     y += dy[idx]
--- venv/Programmers/Backjun/백준 1064_킹.py
+++ venv/Programmers/Backjun/백준 1064_킹.py
@@ -1,18 +1,18 @@
-a,b,c=input().split()
-A=(ord(a[0])-ord('A'),int(a[1])-1)
-B=(ord(b[0])-ord('A'),int(b[1])-1)
+a, b, c = input().split()
+A = (ord(a[0]) - ord('A'), int(a[1]) - 1)
+B = (ord(b[0]) - ord('A'), int(b[1]) - 1)
 
 # 우측, 좌측, 하향, 상향 // 우상향 좌하향 우하향 좌하향
-moves={'R':(1,0),'L':(-1,0),'B':(0,-1),'T':(0,1),
-'RT':(1,1),'LT':(-1,1),'RB':(1,-1),'LB':(-1,-1)}
+moves = {'R': (1, 0), 'L': (-1, 0), 'B': (0, -1), 'T': (0, 1),
+         'RT': (1, 1), 'LT': (-1, 1), 'RB': (1, -1), 'LB': (-1, -1)}
 for i in range(int(c)):
-        move=moves[input()]
-        NA=(A[0]+move[0],A[1]+move[1])
-        if NA==B:
-                NB=(B[0]+move[0],B[1]+move[1])
-        else:
-                NB=B
-        if 0<=NA[0]<8 and 0<=NA[1]<8 and 0<=NB[0]<8 and 0<=NB[1]<8:
-                A,B=NA,NB
-print('%c%d'%(chr(ord('A')+A[0]),1+A[1]))
-print('%c%d'%(chr(ord('A')+B[0]),1+B[1]))
+    move = moves[input()]
+    NA = (A[0] + move[0], A[1] + move[1])
+    if NA == B:
+        NB = (B[0] + move[0], B[1] + move[1])
+    else:
+        NB = B
+    if 0 <= NA[0] < 8 and 0 <= NA[1] < 8 and 0 <= NB[0] < 8 and 0 <= NB[1] < 8:
+        A, B = NA, NB
+print('%c%d' % (chr(ord('A') + A[0]), 1 + A[1]))
+print('%c%d' % (chr(ord('A') + B[0]), 1 + B[1]))
